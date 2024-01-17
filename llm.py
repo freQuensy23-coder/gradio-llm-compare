da2f7f63d8824bf69462966eb04df058163abdf2
alexeymametyev <mametevalex@gmail.com>
2024-01-17 13:25:00 +0300

    [FIX] dim error in llm tokenizer

--- llm.py
+++ llm.py
@@ -17,7 +17,7 @@ model2 = model2.to('cuda')
 def llm1(history: DialogHistory) -> str:
     prompt = history.generate_prompt()
     input_ids = tokenizer1(prompt, return_tensors='pt')
-    gen_ids = model1.generate(**input_ids.to(model1.device), temperature=1, max_new_tokens=64)[0][len(input_ids):]
+    gen_ids = model1.generate(**input_ids.to(model1.device), temperature=1, max_new_tokens=64)[0][len(input_ids[0]):]
     gen_text = tokenizer1.decode(gen_ids)
     return gen_text
 
@@ -25,6 +25,6 @@ def llm1(history: DialogHistory) -> str:
 def llm2(history: DialogHistory) -> str:
     prompt = history.generate_prompt()
     input_ids = tokenizer2(prompt, return_tensors='pt')
-    gen_ids = model2.generate(**input_ids.to(model2.device), temperature=1, max_new_tokens=64)[0][len(input_ids):]
+    gen_ids = model2.generate(**input_ids.to(model2.device), temperature=1, max_new_tokens=64)[0][len(input_ids[0]):]
     gen_text = tokenizer2.decode(gen_ids)
     return gen_text
