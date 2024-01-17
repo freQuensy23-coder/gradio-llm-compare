07409e07ced1fabebfd41098e7900b32e7e30be6
alexeymametyev <mametevalex@gmail.com>
2024-01-17 12:37:22 +0300

    [FIX] button 2 listener

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import gradio as gr
 import callbacks
 
 
-
 with gr.Blocks() as app:
     user_data = gr.State()
     chat_area = gr.Chatbot()
     with gr.Row():
         with gr.Column(scale=1):
-            text1 = gr.Textbox(interactive=False, label='Mistral Answer', lines=3)
+            text1 = gr.Textbox(interactive=False, label='Second answer', lines=3)
             btn1 = gr.Button('Apply first')
         with gr.Column(scale=1):
-            text2 = gr.Textbox(interactive=False, label='Llama Answer', lines=3)
+            text2 = gr.Textbox(interactive=False, label='First answer', lines=3)
             btn2 = gr.Button('Apply second')
     input_area = gr.Textbox(lines=1, label='Text input', placeholder='Type here and press Enter...', interactive=None)
 
@@ -22,7 +21,7 @@ with gr.Blocks() as app:
     btn1.click(fn=callbacks.apply,
                inputs=[input_area, text1, chat_area],
                outputs=[input_area, text1, text2, chat_area])
-    btn1.click(fn=callbacks.apply,
+    btn2.click(fn=callbacks.apply,
                inputs=[input_area, text2, chat_area],
                outputs=[input_area, text1, text2, chat_area])
 
